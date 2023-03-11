8b8a782b664f8aa0d74f089b7e44ccaaeaf8722c
Tuba Tosunoglu <58993751+TUBATOSUNOGLU@users.noreply.github.com>
2023-03-11 23:10:45 +0000

    Update func is added for posts

--- Flask_Blog/flaskblog/routes.py
+++ Flask_Blog/flaskblog/routes.py
@@ -1,7 +1,7 @@
 import os
 import secrets
 
-from flask import flash, redirect, render_template, request, url_for
+from flask import abort, flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required, login_user, logout_user
 from flaskblog import app, bcrypt, db
 from flaskblog.forms import (Loginform, PostForm, Registrationform,
@@ -101,11 +101,32 @@ def new_post():
         db.session.commit()
         flash('Your post has been created!', 'success')
         return redirect(url_for('home'))
-    return render_template('create_post.html', title='New Post', form=form)
+    return render_template('create_post.html', title='New Post', 
+                           form=form, legend='New Post')
 
 
 @app.route("/post/<int:post_id>")
 def post(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('post.html', title=post.title, post=post)
-    
+
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Yor post has been update!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+    form.title.data = post.title
+    form.content.data = post.content
+    return render_template('create_post.html', title='Update Post', 
+                           form=form, legend='Update Post')
