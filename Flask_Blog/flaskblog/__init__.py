5b6793d8f86f3198170309938a89c18d78b59652
Tuba Tosunoglu <58993751+TUBATOSUNOGLU@users.noreply.github.com>
2023-03-11 07:32:09 +0000

    Bcrypt is imported to project. RegistrationForm is updated to save registered user into the db. Validation is added to register page for registered user

--- Flask_Blog/flaskblog/__init__.py
+++ Flask_Blog/flaskblog/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='5c2d06c9304afa76925b4b82ab092102'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
--- Flask_Blog/flaskblog/forms.py
+++ Flask_Blog/flaskblog/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
+from flaskblog.models import User
 from wtforms import BooleanField, PasswordField, StringField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, Length
+from wtforms.validators import (DataRequired, Email, EqualTo, Length,
+                                ValidationError)
 
 
 class Registrationform(FlaskForm):
@@ -14,6 +16,17 @@ class Registrationform(FlaskForm):
                                    validators=[DataRequired(), EqualTo('password')])
     submit=SubmitField('Sign Up')
     
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken, please choose a different one')
+        
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken, please choose a different one')
+    
+        
 class Loginform(FlaskForm):
     email=StringField('Email', 
                       validators=[DataRequired(), Email()])
