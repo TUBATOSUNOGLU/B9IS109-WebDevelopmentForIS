995e98a9a4cb245e3369c9270b9c3cd9eb817f3e
Tuba Tosunoglu <58993751+TUBATOSUNOGLU@users.noreply.github.com>
2023-03-12 17:22:35 +0000

    config.py and create_app func are created and

---
+++ Flask_Blog/flaskblog/config.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Config:
+    SECRET_KEY ='5c2d06c9304afa76925b4b82ab092102'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
