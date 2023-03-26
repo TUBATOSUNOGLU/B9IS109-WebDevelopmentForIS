a917f0dec418002e922c2916e1aa0b8c1396846a
Tuba Tosunoglu <58993751+TUBATOSUNOGLU@users.noreply.github.com>
2023-03-26 14:52:10 +0100

    Image file can be uploaded when new post is created.

--- Flask_Blog/flaskblog/models.py
+++ Flask_Blog/flaskblog/models.py
@@ -18,11 +18,11 @@ class User(db.Model, UserMixin):
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
-    
+
     def get_reset_token(self, expires_sec=1800):
         s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
         return s.dumps({'user_id': self.id}).decode('utf-8').capitalize
-    
+
     @staticmethod
     def verify_reset_token(token):
         s = Serializer(current_app.config['SECRET_KEY'])
@@ -30,11 +30,10 @@ class User(db.Model, UserMixin):
             user_id = s.loads(token)['user_id']
         except:
             return User.query.get(user_id)
-    
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-    
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -42,6 +41,7 @@ class Post(db.Model):
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    image_file = db.Column(db.String(20))
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
