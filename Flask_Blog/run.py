702fd76a683d1dfd8fd843b997a2c344bd7b68a0
Tuba Tosunoglu <58993751+TUBATOSUNOGLU@users.noreply.github.com>
2023-04-05 11:49:52 +0100

    port and flask_env is changed back.

--- Flask_Blog/run.py
+++ Flask_Blog/run.py
@@ -3,7 +3,7 @@ from flaskblog import create_app
 app = create_app()
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 if __name__ == '__main__':
-    app.run(host="127.0.0.1" , port=8000)
+    app.run(debug=True)
 
 
 
